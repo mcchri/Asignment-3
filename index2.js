a79e7aa61e43c907b0fc1e19940d3142713848bc
mcchri <112865223+mcchri@users.noreply.github.com>
2022-09-22 19:16:53 +0100

    Add files via upload

---
+++ index2.js
@@ -0,0 +1,87 @@
+//Decrement Operator
+
+
+
+let k = 5;
+
+
+
+console.log(k--);
+
+
+
+console.log(k)
+
+
+
+console.log(--k)
+
+
+
+//Comparison operators
+
+
+
+//Equals ==
+
+
+
+console.log(5 == 3)
+
+
+
+console.log (4 == 4);
+
+
+
+//not equals !=
+
+
+
+console.log(5!=4)
+
+console.log(4!=4)
+
+
+
+//greater than >
+
+
+
+console.log( 5> 4);
+
+
+
+console.log(4 >5);
+
+
+
+//less than
+
+
+
+console.log(5< 4);
+
+console.log(4<5);
+
+
+
+//Greater than or equal to >=
+
+console.log( 5>= 4);
+
+
+
+console.log(4 >=5);
+
+
+
+//Less than or equal to <=
+
+
+
+console.log( 5<=4);
+
+
+
+console.log(4 <=5);
